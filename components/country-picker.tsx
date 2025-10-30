7b956038cba3830bd29299149b9fccea8117e774
Ovidiu Căpraru <ovidiu.capraru30@gmail.com>
2025-10-30 22:19:46 +0200

    Update country-picker.tsx

--- components/country-picker.tsx
+++ components/country-picker.tsx
@@ -7,7 +7,8 @@ import axios from 'axios';
 import { ChevronDownIcon } from 'lucide-react';
 import { cn } from '@/lib/utils';
 
-const CountryPicker = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => {
+const CountryPicker = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => {
     const { country, setCountry } = useCountry();
     const [countriesList, setCountriesList] = useState<string[]>([]);
     const [open, setOpen] = useState(false);
@@ -15,50 +16,56 @@ const CountryPicker = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTML
     const displayNames = new Intl.DisplayNames(['en'], { type: 'region' });
 
     useEffect(() => {
-        (async () => {
-            const response = await axios.get('/api/get-countries');
-            if (!response.data.success) return;
-            if (response.data.data.length === 0) return;
-            setEnabled(true);
-            setCountriesList(response.data.data);
-            const savedCountry = localStorage.getItem('country');
-            if (!savedCountry || !response.data.data.includes(savedCountry)) setCountry(response.data.data[0]);
-        })();
+      (async () => {
+        const response = await axios.get('/api/get-countries');
+        if (!response.data.success) return;
+        if (response.data.data.length === 0) return;
+        setEnabled(true);
+        setCountriesList(response.data.data);
+        const savedCountry = localStorage.getItem('country');
+        if (!savedCountry || !response.data.data.includes(savedCountry))
+          setCountry(response.data.data[0]);
+      })();
     }, []);
+
     return (
-        <>
-            {enabled && (
-                <div className={cn('flex', className)} ref={ref} {...props}>
-                    <Select value={country} onValueChange={setCountry} open={open} onOpenChange={setOpen}>
-                        <SelectTrigger className='select-none outline-none'>
-                            <div className='bg-background rounded-full'>
-                                <div className='bg-primary/10 flex gap-2 px-3 py-1 rounded-full outline-primary/40 outline-[0.5px] outline items-center justify-center text-nowrap'>
-                                    {country ? (
-                                        <>
-                                            <ReactCountryFlag countryCode={country} />
-                                        </>
-                                    ) : (
-                                        <p>Select a country</p>
-                                    )}
-                                    <ChevronDownIcon />
-                                </div>
-                            </div>
-                        </SelectTrigger>
-                        <SelectContent className='mt-2'>
-                            {countriesList.map((country) => (
-                                <SelectItem key={country} value={country}>
-                                    <div className='flex gap-2 items-center'>
-                                        <ReactCountryFlag countryCode={country} />
-                                        {displayNames.of(country)}
-                                    </div>
-                                </SelectItem>
-                            ))}
-                        </SelectContent>
-                    </Select>
+      <>
+        {enabled && (
+          <div className={cn('flex', className)} ref={ref} {...props}>
+            <Select value={country} onValueChange={setCountry} open={open} onOpenChange={setOpen}>
+              <SelectTrigger className='select-none outline-none'>
+                <div className='bg-background rounded-full'>
+                  <div className='bg-primary/10 flex gap-2 px-3 py-1 rounded-full outline-primary/40 outline-[0.5px] outline items-center justify-center text-nowrap'>
+                    {country ? (
+                      <>
+                        <ReactCountryFlag countryCode={country} />
+                      </>
+                    ) : (
+                      <p>Select a country</p>
+                    )}
+                    <ChevronDownIcon />
+                  </div>
                 </div>
-            )}
-        </>
+              </SelectTrigger>
+              <SelectContent className='mt-2'>
+                {countriesList.map((country) => (
+                  <SelectItem key={country} value={country}>
+                    <div className='flex gap-2 items-center'>
+                      <ReactCountryFlag countryCode={country} />
+                      {displayNames.of(country)}
+                    </div>
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+        )}
+      </>
     );
-});
+  }
+);
+
+// 👇 Adaugă acest rând
+CountryPicker.displayName = 'CountryPicker';
 
 export default CountryPicker;
